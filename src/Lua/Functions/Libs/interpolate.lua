e5e1ddf8f37981d75135a44c21cb114fd6806018
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-10-03 22:29:56 -0500

    add safety checks to ML.interpolate

--- src/Lua/Functions/Libs/interpolate.lua
+++ src/Lua/Functions/Libs/interpolate.lua
@@ -1,4 +1,7 @@
 return function(v, set)
+	if type(v) == "boolean"
+		error("MenuLib.interpolate(drawer v, tag) :: argument 1 must be drawer table")
+	end
 	if (v.interpolate == nil) then return end
 	
 	v.interpolate(set)
